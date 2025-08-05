e6898f4bdf1b0bdd22b9e38a4d158ae3d9b6abeb
Abbby1007 <Abigailloken25@gmail.com>
2025-08-05 13:36:47 -0400

    update enemy animation

--- enemy.gd
+++ enemy.gd
@@ -8,6 +8,15 @@ func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position)/speed
 		
+		$AnimatedSprite2D.play("walk")
+		
+		if(player.position.x - position.x)<0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.play("idle")
+		
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
